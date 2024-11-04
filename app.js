8cf4606b103fc111de5536fbaa5556271df2f081
thais36 <thais.almeida14@fatec.sp.gov.br>
2024-11-03 23:39:45 -0300

    Evento sendo trabalhado...

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const handlebars = require("express-handlebars").engine;
 const bodyParser = require("body-parser");
 const { Modalidade, Inscricao, Contato, Loja, Evento } = require("./models/post");
 
+// Configuração para servir arquivos estáticos
+app.use(express.static("public"));
+
 // Configuração do handlebars
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.engine("handlebars", handlebars({
+    defaultLayout: "main",
+    helpers: {
+        formatDate: function(date) {
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            return new Date(date).toLocaleDateString('pt-BR', options);
+        }
+    }
+}));
 app.set("view engine", "handlebars");
 
 // Configuração do body-parser
@@ -17,6 +28,17 @@ app.use((req, res, next) => {
     res.locals.currentPath = req.path;
     next();
 });
+
+// Função auxiliar para buscar dados e renderizar views
+const renderData = (model, res, viewName) => {
+    model.findAll()
+        .then((data) => {
+            res.render(viewName, { data });
+        })
+        .catch((erro) => {
+            res.send(`Erro ao carregar ${viewName}: ` + erro);
+        });
+};
 
 // Rota principal (home) - Exibe modalidades e link para inscrição
 app.get("/", (req, res) => {
@@ -49,16 +71,12 @@ app.post("/cadastrarContato", (req, res) => {
 
 // Rotas de exibição para Evento - alunos e demais interessados
 app.get("/evento", (req, res) => {
-    Evento.findAll()
-        .then((eventos) => res.render("evento", { eventos }))
-        .catch((erro) => res.send("Erro ao carregar eventos: " + erro));
+    renderData(Evento, res, "evento");
 });
 
 // Rotas de exibição para Loja - alunos e demais interessados
 app.get("/loja", (req, res) => {
-    Loja.findAll()
-        .then((lojas) => res.render("loja", { lojas }))
-        .catch((erro) => res.send("Erro ao carregar lojas: " + erro));
+    renderData(Loja, res, "loja");
 });
 
 // Página de gestão do site com links para os CRUDs de cada seção - Membros da gestão da atlética
@@ -92,33 +110,22 @@ app.post("/cadastrarModalidade", (req, res) => {
 });
 
 app.get("/consultarModalidade", (req, res) => {
-    Modalidade.findAll()
-        .then((modalidades) => {
-            res.render("consultarModalidade", { modalidades });
-        })
-        .catch((erro) => res.send("Erro ao carregar as modalidades: " + erro));
+    renderData(Modalidade, res, "consultarModalidade");
 });
-
 
 // Rotas para CRUD de Inscrições
 app.get("/consultarInscricao", (req, res) => {
-    Inscricao.findAll()
-        .then((inscricoes) => res.render("consultarInscricao", { inscricoes }))
-        .catch((erro) => res.send("Erro ao carregar inscrições: " + erro));
+    renderData(Inscricao, res, "consultarInscricao");
 });
 
 // Rotas para CRUD de Contatos
 app.get("/consultarContato", (req, res) => {
-    Contato.findAll()
-        .then((contatos) => res.render("consultarContato", { contatos }))
-        .catch((erro) => res.send("Erro ao carregar contatos: " + erro));
+    renderData(Contato, res, "consultarContato");
 });
 
 // Rotas para CRUD da Loja
 app.get("/consultarLoja", (req, res) => {
-    Loja.findAll()
-        .then((lojas) => res.render("consultarLoja", { lojas }))
-        .catch((erro) => res.send("Erro ao carregar lojas: " + erro));
+    renderData(Loja, res, "consultarLoja");
 });
 
 app.get("/cadastrarLoja", (req, res) => {
@@ -133,9 +140,7 @@ app.post("/cadastrarLoja", (req, res) => {
 
 // Rotas para CRUD de Eventos
 app.get("/consultarEvento", (req, res) => {
-    Evento.findAll()
-        .then((eventos) => res.render("consultarEvento", { eventos }))
-        .catch((erro) => res.send("Erro ao carregar eventos: " + erro));
+    renderData(Evento, res, "consultarEvento");
 });
 
 app.get("/cadastrarEvento", (req, res) => {
@@ -143,9 +148,50 @@ app.get("/cadastrarEvento", (req, res) => {
 });
 
 app.post("/cadastrarEvento", (req, res) => {
+    const eventoData = new Date(req.body.data); // Supondo que 'data' esteja no corpo da requisição
+    const now = new Date();
+
+    if (eventoData <= now) {
+        return res.send("Erro: A data do evento deve ser uma data futura.");
+    }
+
     Evento.create(req.body)
         .then(() => res.redirect("/consultarEvento"))
         .catch((erro) => res.send("Erro ao cadastrar evento: " + erro));
+});
+
+// Rota para exibir o formulário de edição de um evento específico
+app.get("/editarEvento/:id", (req, res) => {
+    Evento.findByPk(req.params.id)
+        .then((evento) => {
+            if (evento) {
+                res.render("editarEvento", { evento });
+            } else {
+                res.send("Evento não encontrado.");
+            }
+        })
+        .catch((erro) => res.send("Erro ao buscar evento: " + erro));
+});
+
+// Rota para processar a edição de um evento
+app.post("/editarEvento/:id", (req, res) => {
+    const eventoData = new Date(req.body.data);
+    const now = new Date();
+
+    if (eventoData <= now) {
+        return res.send("Erro: A data do evento deve ser uma data futura.");
+    }
+
+    Evento.update(req.body, { where: { id: req.params.id } })
+        .then(() => res.redirect("/consultarEvento"))
+        .catch((erro) => res.send("Erro ao editar evento: " + erro));
+});
+
+// Rota para deletar um evento
+app.post("/deletarEvento/:id", (req, res) => {
+    Evento.destroy({ where: { id: req.params.id } })
+        .then(() => res.redirect("/consultarEvento"))
+        .catch((erro) => res.send("Erro ao deletar evento: " + erro));
 });
 
 // Inicialização do servidor
